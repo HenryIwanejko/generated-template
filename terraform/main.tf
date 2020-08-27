d6be75649910f3335fc686038a74a73e6f60e6e5
Henry Iwanejko <henry.iwanejko1@nhs.net>
2020-08-27 11:04:21 +0100

    APM-1214

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+provider "apigee" {
+  org          = var.apigee_organization
+  access_token = var.apigee_token
+}
+
+terraform {
+  backend "azurerm" {}
+
+  required_providers {
+    apigee = "~> 0.0"
+    archive = "~> 1.3"
+  }
+}
+
+
+module "generated-template" {
+  source                   = "github.com/NHSDigital/api-platform-service-module"
+  name                     = "generated-template"
+  path                     = "generated-template"
+  apigee_environment       = var.apigee_environment
+  proxy_type               = (var.force_sandbox || length(regexall("sandbox", var.apigee_environment)) > 0) ? "sandbox" : "live"
+  namespace                = var.namespace
+  make_api_product         = !(length(regexall("sandbox", var.apigee_environment)) > 0)
+  api_product_display_name = length(var.namespace) > 0 ? "generated-template${var.namespace}" : "Generated Template"
+  api_product_description  = "This is a generated template API"
+}
